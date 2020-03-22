96a4920b782ecc1f101cd31db78251ee32a6ad58
Goutam Bose <goutam.bose@keysight.com>
2020-03-22 12:31:18 +0530

    Incorporating total cases too

--- Scrapy.py
+++ Scrapy.py
@@ -15,7 +15,7 @@ class currentSituation:
                 table_body = html_soup.find_all('tbody')[1]
                 tr_list    = table_body.find_all('tr')
 
-                for i in tr_list[:-1]:
+                for i in tr_list:
                     td_list = i.find_all('td')
                     if len(td_list) == 6:
                         state, indian, foreign, cured, died = td_list[1:]
@@ -25,6 +25,14 @@ class currentSituation:
                         cured   = int(cured.text)
                         died    = int(died.text)
                         self.data_set[state] = [indian, foreign, cured, died]
+                    if len(td_list) == 5:
+                        total_cases_text, indian, foreign, cured, died = td_list
+                        total_cases_text = str(total_cases_text.text)
+                        indian  = int(indian.text)
+                        foreign = int(foreign.text)
+                        cured   = int(cured.text)
+                        died    = int(died.text)
+                        self.data_set[total_cases_text] = [indian, foreign, cured, died]
             else:
                 self.response_rcvd = False
         except:
